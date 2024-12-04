ab0cd7c894601ef5c42e6b04e4af594f68948b16
ydh5017 <ydh5017@naver.com>
2024-12-04 22:40:15 +0900

    init commit

---
+++ src/main/java/com/project/websockat/WebsockatApplication.java
@@ -0,0 +1,13 @@
+package com.project.websockat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsockatApplication.class, args);
+    }
+
+}
